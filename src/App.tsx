d408f284e5d2e7e9d90e6e11c0204f7835c69219
kira0826 <curaca492@gmail.com>
2024-10-31 15:29:31 -0500

    hello world console log

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import {  Typography } from "@material-tailwind/react";
 
 
 function App() {
+
+
+  console.log('hello sadadasd 
+    
+    
+    world');
+
   return (
     <div>
       <h1>Hola mi papa</h1>
