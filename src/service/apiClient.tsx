166441ac0c369419731b74c619108eb127c6de28
Alejolonber25 <alejandrolonber25@gmail.com>
2024-11-03 01:40:53 -0500

    build: sign in page build

--- src/service/apiClient.tsx
+++ src/service/apiClient.tsx
@@ -1,16 +1,17 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:8080/', //must define correct API URL  
+  baseURL: 'http://127.0.0.1:8081/', //must define correct API URL  
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: true,
 });
 
 apiClient.interceptors.request.use((config) => {
   const token = sessionStorage.getItem('token');  
   if (token) {
-    config.headers.Authorization = `Token ${token}`; 
+    config.headers.Authorization = `${token}`; 
   }
   return config;
 });
