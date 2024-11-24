b4e42bd22b8529c63763fe76178af69eb5059199
kira0826 <curaca492@gmail.com>
2024-11-23 22:13:06 -0500

    refactor: export perform function.

--- src/pages/play/play.tsx
+++ src/pages/play/play.tsx
@@ -8,15 +8,12 @@ import { Piece } from "@/widgets";
 import { LastMove } from "@/widgets/chess/types";
 import { PieceType } from "@/widgets";
 import {
-  movePiece,
-  handleCastling,
-  handleEnPassantCapture,
-  updateEnPassantEligibility,
   isKingInCheck,
   isPromotion,
 } from "@/widgets/chess/boardAuxFunctions";
 import isValidMove from "@/validations/isValidMove";
 import StompService from "@/service/webSocketService";
+import { performMove } from "./playAux";
 
 export function Play() {
 
@@ -33,9 +30,6 @@ export function Play() {
       });
 
   }, [])
-
-
-
 
   const [boardSetup, setBoardSetup] = useState<(Cell | null)[][]>(
     initialBoardSetup()
@@ -70,7 +64,6 @@ export function Play() {
     toRow: number,
     toCol: number
   ) => {
-    // Actualizar el estado del tablero y el último movimiento
     setBoardSetup(newBoard);
     setLastMove({
       piece: piece,
@@ -81,36 +74,7 @@ export function Play() {
     });
   };
 
-  const performMove = (
-    board: (Cell | null)[][],
-    piece: Piece,
-    fromRow: number,
-    fromCol: number,
-    toRow: number,
-    toCol: number
-  ): (Cell | null)[][] => {
-    let newBoard = board.map((boardRow) => boardRow.slice());
 
-    // Mover la pieza
-    newBoard = movePiece(newBoard, fromRow, fromCol, toRow, toCol, piece);
-
-    // Manejar captura al paso
-    newBoard = handleEnPassantCapture(newBoard, piece, fromCol, toRow, toCol);
-
-    // Actualizar elegibilidad de en passant
-    newBoard = updateEnPassantEligibility(
-      newBoard,
-      piece,
-      fromRow,
-      toRow,
-      toCol
-    );
-
-    // Manejar enroque
-    newBoard = handleCastling(newBoard, piece, fromCol, toRow, toCol);
-
-    return newBoard;
-  };
 
   const handlePromotion = (promotedPieceType: PieceType) => {
     if (pendingPromotion) {
@@ -214,11 +178,6 @@ export function Play() {
       </main>
 
       <section className="flex flex-col h-5/6 w-1/3 bg-gray-200">
-
-
-
-
-      
       </section>
 
       {openCoronation && (
---
+++ src/pages/play/playAux.ts
@@ -0,0 +1,39 @@
+import { Cell } from "@/widgets/chess/types";
+import { Piece } from "@/widgets";
+import {
+    movePiece,
+    handleCastling,
+    handleEnPassantCapture,
+    updateEnPassantEligibility
+  } from "@/widgets/chess/boardAuxFunctions";
+
+export const performMove = (
+    board: (Cell | null)[][],
+    piece: Piece,
+    fromRow: number,
+    fromCol: number,
+    toRow: number,
+    toCol: number
+  ): (Cell | null)[][] => {
+    let newBoard = board.map((boardRow) => boardRow.slice());
+
+    // Mover la pieza
+    newBoard = movePiece(newBoard, fromRow, fromCol, toRow, toCol, piece);
+
+    // Manejar captura al paso
+    newBoard = handleEnPassantCapture(newBoard, piece, fromCol, toRow, toCol);
+
+    // Actualizar elegibilidad de en passant
+    newBoard = updateEnPassantEligibility(
+      newBoard,
+      piece,
+      fromRow,
+      toRow,
+      toCol
+    );
+
+    // Manejar enroque
+    newBoard = handleCastling(newBoard, piece, fromCol, toRow, toCol);
+
+    return newBoard;
+  };
